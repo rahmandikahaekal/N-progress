00d9efe2c5466d268d6623401473dc4aeb0941b2
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2019-04-04 08:13:56 +0700

    upload all

---
+++ asset/js/custom.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+    siteFooter();
+
+    $(window).resize(function() {
+      siteFooter();
+    });
+
+    function siteFooter() {
+      var siteContent = $('#site-content');
+      var siteContentHeight = siteContent.height();
+      var siteContentWidth = siteContent.width();
+
+      var siteFooter = $('#site-footer');
+      var siteFooterHeight = siteFooter.height();
+      var siteFooterWidth = siteFooter.width();
+
+      console.log('Content Height = ' + siteContentHeight + 'px');
+      console.log('Content Width = ' + siteContentWidth + 'px');
+      console.log('Footer Height = ' + siteFooterHeight + 'px');
+      console.log('Footer Width = ' + siteFooterWidth + 'px');
+
+      siteContent.css({
+        "margin-bottom" : siteFooterHeight + 50
+      });
+    };
+
+    $('.play-btn').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+
+  });
+
+  $(function() {
+    $('#nav li a').click(function() {
+       $('#nav li').removeClass();
+       $($(this).attr('href')).addClass('active');
+    });
+  });
+
